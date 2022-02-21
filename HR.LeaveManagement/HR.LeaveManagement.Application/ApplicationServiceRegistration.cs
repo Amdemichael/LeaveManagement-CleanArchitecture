a0b25aa6f6b6bb366cfb820a469a59c4f68d88b0
amde <AMKidane@excellerentsolutions.com>
2022-02-21 11:45:56 +0300

    Add Dtos and auto mapper to application core layer

---
+++ HR.LeaveManagement/HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using HR.LeaveManagement.Application.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace HR.LeaveManagement.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
